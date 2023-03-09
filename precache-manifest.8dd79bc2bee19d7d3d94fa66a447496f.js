b5c5979089c7a6206f4d172a1671669edcf20e6a
dancreactive23 <dan.creactive23@gmail.com>
2023-03-09 13:34:18 -0500

    Updates

---
+++ precache-manifest.8dd79bc2bee19d7d3d94fa66a447496f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e2219f85371bcbbbe59",
+    "url": "/robofriends/static/css/main.a1c9f1af.chunk.css"
+  },
+  {
+    "revision": "8e2219f85371bcbbbe59",
+    "url": "/robofriends/static/js/main.1d374786.chunk.js"
+  },
+  {
+    "revision": "cb45653fdb1cd181cc7c",
+    "url": "/robofriends/static/js/runtime~main.465d8a73.js"
+  },
+  {
+    "revision": "9f716ed50720d796579c",
+    "url": "/robofriends/static/css/2.6c4a85f0.chunk.css"
+  },
+  {
+    "revision": "9f716ed50720d796579c",
+    "url": "/robofriends/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2c52018a651ea7d447fc8d40c707ee4d",
+    "url": "/robofriends/index.html"
+  }
+];
